3bc6001af8555b02055fcf91c67d1916e8e11348
SSypp <2435884205@qq.com>
2019-10-24 18:25:17 +0800

    fetch-jsonp

--- src/components/Movie/movie.jsx
+++ src/components/Movie/movie.jsx
@@ -14,24 +14,42 @@ export default class Movie extends Component{
         super(props)
     }
 
+    componentWillMount() {
+        // fetch("http://www.liulongbin.top:3005/api/getlunbo")
+        //     .then(
+        //         res => {
+        //             return res.json()
+        //         }
+        //     ).then(
+        //         data => {
+        //             console.log(data)
+        //         }
+        // )
+    }
+    // shouldComponentUpdate(nextProps, nextState, nextContext) {
+    //     //     console.log(window.location.hash.split("/"))
+    //     // }
+    // static getDerivedStateFromProps(nextProps, prevState){
+    //     console.log(window.location.hash.split("/"))
+    //     return null;
+    // }
     render(){
         return(
             <Layout style={{height:"100%"}}>
                 <Sider width={200} style={{ background: '#fff' }}>
                     <Menu
                         mode="inline"
-                        defaultSelectedKeys={['1']}
-                        defaultOpenKeys={['sub1']}
+                        defaultSelectedKeys={window.location.hash.split("/")[2]}
                         style={{ height: '100%', borderRight: 0 }}
                     >
-                        <Menu.Item key="1"><Link to="/movie/in_theaters/1">正在热映</Link></Menu.Item>
-                        <Menu.Item key="2"><Link to="/movie/coming_soon/1">即将上映</Link></Menu.Item>
-                        <Menu.Item key="3"><Link to="/movie/top250/1">Top250</Link></Menu.Item>
+                        <Menu.Item key="in_theaters"><Link to="/movie/in_theaters/1">正在热映</Link></Menu.Item>
+                        <Menu.Item key="coming_soon"><Link to="/movie/coming_soon/1">即将上映</Link></Menu.Item>
+                        <Menu.Item key="top250"><Link to="/movie/top250/1">Top250</Link></Menu.Item>
                     </Menu>
                 </Sider>
                 <Layout>
                     <Content>
-                        <Route path="/movie/:type/:id" component={MovieList}></Route>
+                        <Route path="/movie/:type/:page" component={MovieList}></Route>
                     </Content>
                 </Layout>
             </Layout>
--- src/components/Movie/movieView/MovieList.jsx
+++ src/components/Movie/movieView/MovieList.jsx
@@ -1,16 +1,63 @@
 import React,{Component} from "react"
 
+import fetchJsonp from "fetch-jsonp"
+
+import { Spin, Alert } from 'antd';
 export default class MovieList extends Component{
     constructor(props){
         super(props)
+        this.state = {
+            movies:[],//当前电影列表
+            nowPage:parseInt(props.match.params.page) || 1,//当前电影所在的页
+            pageSize:10,//每页显示多少条数据
+            totalCount:14,//总电影数
+            isLoading: true, //是否加载loading
+            movieType:props.match.params.type //电影的类型
+        }
     }
 
     render(){
         console.log(this.props.match.params)
         return (
             <div>
-                我是热门排行榜--{this.props.match.params.type} -- {this.props.match.params.id}
+                {this.reLoadList()}
             </div>
         )
+    }
+
+    componentWillMount() {
+        setTimeout(()=>{
+            this.setState({
+                isLoading: false
+            })
+        },2000)
+
+        // fetchJsonp("http://api.douban.com/v2/movie/in_theaters?apikey=0df993c66c0c636e29ecbb5344252a4a").then(
+        //     res => {
+        //         return res.json()
+        //     }
+        // ).then(
+        //     data => {
+        //         console.log(data)
+        //     }
+        // )
+    }
+
+    reLoadList = () => {
+        if(this.state.isLoading){
+            return (
+                <Spin tip="Loading...">
+                    <Alert
+                        message="Alert message title"
+                        description="Further details about the context of this alert."
+                        type="info"
+                    />
+                </Spin>
+            )
+        }else{
+            return (
+                <h1>页面已经加载完成！！</h1>
+            )
+        }
     }
 }
