8954ef46dd2bc30a36a608113da629f498655eb0
SSypp <2435884205@qq.com>
2019-10-25 08:26:08 +0800

    获取电影数据

--- src/components/Movie/movieView/MovieList.jsx
+++ src/components/Movie/movieView/MovieList.jsx
@@ -12,7 +12,8 @@ export default class MovieList extends Component{
             pageSize:10,//每页显示多少条数据
             totalCount:14,//总电影数
             isLoading: true, //是否加载loading
-            movieType:props.match.params.type //电影的类型
+            movieType:props.match.params.type//电影的类型
+
         }
     }
 
@@ -26,17 +27,26 @@ export default class MovieList extends Component{
     }
 
     componentWillMount() {
-        setTimeout(()=>{
-            this.setState({
-                isLoading: false
-            })
-        },2000)
+        // setTimeout(()=>{
+        //     this.setState({
+        //         isLoading: false
+        //     })
+        // },2000)
 
         // fetchJsonp("http://api.douban.com/v2/movie/in_theaters?apikey=0df993c66c0c636e29ecbb5344252a4a").then(
         //     res => {
         //         return res.json()
         //     }
         // ).then(
+        //     data => {
+        //         console.log(data)
+        //     }
+        // )
+
+        // let start = this.state.pageSize * (this.state.nowPage - 1)
+        // let url = `http://api.douban.com/v2/movie/${this.props.match.params.type}?apikey=0df993c66c0c636e29ecbb5344252a4a&start=${start}&count=${this.state.pageSize}`
+        //
+        // fetchJsonp(url).then(res => {return res.json()}).then(
         //     data => {
         //         console.log(data)
         //     }
